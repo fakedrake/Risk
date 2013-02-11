b56be336cacfdb8863cca3a8dc90b9e3316351ab
patatahooligan <filwn.oikonomou@gmail.com>
2013-02-11 23:47:35 +0200

    Added get_user_input method

--- risk/gui.py
+++ risk/gui.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 
 class Gui(object):
     """Draws the board from image in resources. Gets user input and updates the
@@ -20,3 +21,28 @@ class Gui(object):
         window = pygame.display.set_mode (board_image.get_size())
         window.blit(board_image, (0,0))
         pygame.display.update()
+
+        pygame.event.set_allowed(None)    #Ignore all events
+        pygame.event.set_allowed(MOUSEBUTTONDOWN)   #Except mouse clicks
+
+    def get_user_input(self):
+        """Get user input and return it in meaningful way (for example region obj)
+        """
+
+        # If an event has already occured, we assume it happened while the game
+        # was not expecting it so it makes sense to ignore it
+        pygame.event.clear()
+        click = pygame.event.wait()
+        return self.evaluate_input(click)
+
+    def evaluate_input(self, click):
+        """Evaluates user input and returns something the game loop can understand
+        
+        Arguments:
+        - `click`: MOUSEBUTTONDOWN event to be evaluated
+        """
+
+        # Need to implement a mapping system to recognize what a given mouse
+        # position needs
+
+        return None
